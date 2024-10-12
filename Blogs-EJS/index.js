1d264792be72b075c6bafba594697f8842774c75
Nagesh Jayaram <nagesh.jayaram@firecompass.com>
2024-10-12 18:30:26 +0530

    added UUID Id generator and also changed the ID to Str

--- Blogs-EJS/index.js
+++ Blogs-EJS/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import fs from "fs"
 import bodyParser from "body-parser";
+import { randomUUID } from "crypto";
 
 import {dirname} from "path"
 import {fileURLToPath} from "url"
 const app = express();
 const port = 3000;
 
+console.log(randomUUID())
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }));
 /* Write your code here:
@@ -17,22 +19,26 @@ Step 3: Add the routes to handle the render of the about and contact pages.
 Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
 
 var blogs = [
-  {"title" : "NJ-Blog1","body" : "THis is First Blog 1", "id" : 200},
-  {"title" : "NJ-Blog2","body" : "THis is First Blog 2", "id" : 201},
-  {"title" : "NJ-Blog3","body" : "THis is First Blog 3 this is going to", "id" : 202},
-  {"title" : "NJ-Blog4","body" : "THis is First Blog 4", "id" : 203}
+  {"title" : "NJ-Blog1","body" : "THis is First Blog 1", "id" : '200'},
+  {"title" : "NJ-Blog2","body" : "THis is First Blog 2", "id" : '201'},
+  {"title" : "NJ-Blog3","body" : "THis is First Blog 3 this is going to", "id" : '202'},
+  {"title" : "NJ-Blog4","body" : "THis is First Blog 4", "id" : '203'}
 ]
 
 function delete_id(id) {
   console.log("Came into Function with Id" + id )
   console.log(typeof(id))
+  let found=0
   for(let i=0; i<blogs.length ; i++) {
     console.log(`ID In the Loop with Index ${i} = ${blogs[i]['id']}`)
     if (blogs[i]['id'] === id ) {
       console.log("CAME INSIDE IF MATCHING CONDITION")
        blogs.splice(i,1)
+       found=1;
+       break;
     }
   }
+  if ( ! found) console.log("Cound Find the Blog with Id " + id)
 }
 
 function BlogObj (title, id, blog) {
@@ -60,7 +66,8 @@ app.get("/contact", (req,res) => {
 
 app.post("/delete/:id", (req,res) => {
   console.log(`Request Id Passed from Delete Button is ${req.params.id}`)
-  delete_id(parseInt(req.params.id));
+  // delete_id(parseInt(req.params.id));
+  delete_id(req.params.id);
   res.redirect("/blogs")
 });
 
@@ -68,7 +75,8 @@ app.post("/addblog" , (req,res) => {
   console.log(`Title Passed = ${req.body.title}`);
   console.log(`Blog Passed = ${req.body.blog}`);
 let id = (Math.floor(Math.random() * 100000000));
-var blog = new BlogObj (req.body.title,id,req.body.blog);
+// var blog = new BlogObj (req.body.title,id,req.body.blog);
+var blog = new BlogObj (req.body.title,randomUUID(),req.body.blog);
 console.log(blog)
 blogs.push(blog);
 res.redirect("/blogs")
